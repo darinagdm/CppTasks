2a46f9f892e89c3102d18181f4f0865594a432e3
Daria Gudemchuk <91084561+darinagdm@users.noreply.github.com>
2021-10-12 00:18:41 +0300

    Update HW4(1)_3.10b.cpp

--- HW4(1)_3.10b.cpp
+++ HW4(1)_3.10b.cpp
@@ -6,10 +6,12 @@ int main()
     int x,y,z;
     cin >> x >> y >> z;
     int k = 0;
-    if (x==y && y==z)
+    if (x==y && y==z){
         k = 3;
-    else if ((x==y && x!=z) || (y==z && x!=y) || (x==z && x!=y))
+    }
+    else if ((x==y && x!=z) || (y==z && x!=y) || (x==z && x!=y)){
         k = 2;
+    }
     cout <<k <<endl;
     return 0;
 }
