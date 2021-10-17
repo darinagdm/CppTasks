04a2bce851d6f760ad8b08bbaac35ac8e6b5b781
Daria Gudemchuk <91084561+darinagdm@users.noreply.github.com>
2021-10-17 17:54:14 +0300

    Classwork 8
    Classwork 8

---
+++ CW8_6.5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+unsigned seq_bits(unsigned n){
+   unsigned i=1u;
+   unsigned r=0;
+   unsigned max_r=0;
+   for(;i<=n;i<<=1){
+       if(i&n){
+           r++;
+       }
+       else{
+           if(max_r<r){
+               max_r=r;
+           }
+           r=0;
+       }
+   }
+   return max_r;
+}
+
+int main(){
+    unsigned n;
+    printf("n=");
+    scanf("%u",&n);
+    printf("Number of nonzero sequantial bits in %x is %u",n,seq_bits(n));
+}
