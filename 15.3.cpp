b773b93a095f872a0a2724532809a4d9d3887b9a
Daria Gudemchuk <91084561+darinagdm@users.noreply.github.com>
2021-12-08 19:30:37 +0200

    Update 15.3.cpp

--- 15.3.cpp
+++ 15.3.cpp
@@ -1,92 +1,108 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
-class Polynom {
-    private:
-        int n;
-        double* members;
-
-    public:
-        double &operator[](int i){
-            return members[i];
+class Polynomial{
+private:
+   int coef[100]; 
+   int deg;       
+public:
+    Polynomial::Polynomial(){
+        for (int i = 0; i < 100; i++){
+            coef[i] = 0;
         }
-        friend ostream& operator<<(ostream &out, const Polynom &p) {
-            for (int i = 0; i < p.n; i++) {
-                if (i == 0) {
-                    cout << p.members[i];
-                    continue;
-                } 
-                
-                else {
-                    cout << " + " << p.members[i];
-                }
-                
-                cout << "*x";
-                if (i != 1) cout << "^" << i;
+    }
+    void set(int a, int b){
+        coef[b] = a;
+        deg = degree();
+    }
+    int degree(){
+        int d = 0;
+        for (int i = 0; i < 100; i++){
+            if ( coef[i] != 0 ){ 
+                d = i;
             }
         }
-
-        friend istream &operator>>(istream &in, Polynom &p) {
-            cout << "\nn = ";
-            int item;
-            cin >> item;
-            p.n = item;
-            p.members = (double*)malloc(sizeof(double)*p.n);
-            for (int i=0; i<p.n; i++) {
-                cout << "Member of degree " << i <<": ";
-                cin >> p.members[i];
+        return d;
+    }
+    void print(){
+        for(int i = 99; i >= 0; i--){
+            if(coef[i] != 0){
+                cout << coef[i] << "x^" << i << " ";
             }
         }
-
-        Polynom(){};
-
-        Polynom(int n, double* arr) {
-            this->n = n;
-            members = (double*)malloc(sizeof(double)*n);
-            
-            for (int i = 0; i < n; i++) {
-                members[i] = arr[i];
-            }
-        };
-
-        Polynom(const Polynom &p) {
-            n = p.n;
-            
-            for (int i = 0; i < n; i++) {
-                members[i] = p.members[i];
+    }
+    int evaluate ( int x ){
+        int p = 0;
+        for(int i = deg; i >= 0; i--){
+            p = coef[i] + ( x * p );
+        }
+        return p;
+    }
+    Polynomial differentiate(){
+        if(deg == 0){
+            Polynomial t;
+            t.set (0, 0);
+            return t;
+        }
+        Polynomial deriv;
+        deriv.deg = deg - 1;
+        for(int i = 0; i < deg; i++){
+            deriv.coef[i] = (i + 1) * coef[i + 1];
+        }
+        return deriv;
+    }
+    Polynomial plus ( Polynomial b ){
+        Polynomial a = *this;
+        Polynomial c;
+        for(int i = 0; i <= a.deg; i++){ 
+            c.coef[i] += a.coef[i];
+        }
+        for(int i = 0; i <= b.deg; i++){ 
+            c.coef[i] += b.coef[i];
+        }
+        c.deg = c.degree();
+        return c;
+    }
+    Polynomial minus ( Polynomial b ){
+        Polynomial a = *this; 
+        Polynomial c;
+        for(int i = 0; i <= a.deg; i++){ 
+            c.coef[i] += a.coef[i];
+        }
+        for(int i = 0; i <= b.deg; i++){ 
+            c.coef[i] -= b.coef[i];
+        }
+        c.deg = c.degree();
+        return c;
+    }
+    Polynomial times ( Polynomial b ){
+        Polynomial a = *this; 
+        Polynomial c;
+        for(int i = 0; i <= a.deg; i++){
+            for(int j = 0; j <= b.deg; j++){
+                c.coef[i+j] += (a.coef[i] * b.coef[j]);
             }
         }
-
-        ~Polynom(){free(members);};
-
-        friend Polynom finPoly(const char* fname);
-        friend void foutPoly(const char* fname, const Polynom &p);
+        c.deg = c.degree();
+        return c;
+    }
 };
 
-Polynom finPoly(const char* fname) {
-    FILE* file = fopen(fname, "rb");
-    Polynom p;
-    fread(&p.n, sizeof(p.n), 1, file);
-    p.members = (double*)malloc(sizeof(double)*p.n);
-    fread(&p.members, sizeof(p.members), 1, file);
-    fclose(file);
-    return p;
-}
-
-
-void foutPoly(const char* fname, const Polynom &p) {
-    FILE* file = fopen(fname, "wb");
-    fwrite(&p.n, sizeof(p.n), 1, file);
-    fwrite(&p.members, sizeof(p.members), 1, file);
-    fclose(file);
-}
-
-
-int main() {
-    Polynom p1, p2;
-    cin >> p1;
-    foutPoly("15.3", p1);
-    p2 = finPoly("15.3");
-    cout << p2;
-}
+int main(){
+   Polynomial a, b, c, d;
+   a.set (7, 4); 
+   a.set (1, 2); 
+   b.set (6, 3); 
+   b.set (-3, 2); 
+   c = a.minus (b); 
+   c.print();
+   cout << "\n";
+   c = a.times (b); 
+   c.print();
+   cout << "\n";
+   d = c.differentiate().differentiate();
+   d.print();
+   cout << "\n";
+   cout << c.evaluate (2); 
+   cin.get();
+} 
